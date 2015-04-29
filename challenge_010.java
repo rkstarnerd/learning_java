f7b67605d4789a6a589912c82f0caf112213dc77
Gemille Ford <gemille.ford@gmail.com>
2015-04-29 16:21:50 -0400

    add boilerplate code

--- challenge_010.java
+++ challenge_010.java
@@ -1,10 +1,26 @@
 //Find the factorial of a by multiplying all the numbers from 1 to a together,
-//and then return the total product. For example, when given a as 4, return 24.
+//and then return the total product.
 
-static int doStuff(int a){
-  int product = 1;
-  for(int i=1; i<=a; i=i+1){
-    product = product * i;
+import java.util.Scanner;
+
+public class Main {
+
+  public static int doStuff(int a){
+    int product = 1;
+    for(int i=1; i<=a; i=i+1){
+      product = product * i;
+    }
+    return product;
   }
-  return product;
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+
+    int n = in.nextInt();
+    for(int i=0; i<n; i++){
+      int a = in.nextInt();
+      int result = doStuff(a);
+      System.out.println(result);
+    }
+  }
 }
